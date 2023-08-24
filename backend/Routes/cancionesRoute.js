f691fdf2a4e3138c4ad7b03597d11b1c4f096e23
Tomas luquez <luquez1431@gmail.com>
2023-08-24 17:05:46 -0300

    cambie el backend

--- backend/Routes/cancionesRoute.js
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express';
-import { createCancion, getCanciones } from '../Handlers/hanlders.js';
-const router = Router();
-
-router.post('/', createCancion).get('/', getCanciones);
-
-export { router as cancionesRoutes };
--- backend/Routes/invitadosRoute.js
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express';
-import { createInvitado, getInvitados } from '../Handlers/hanlders.js';
-const router = Router();
-
-router.post('/', createInvitado).get('/', getInvitados);
-
-export { router as invitadosRoutes };

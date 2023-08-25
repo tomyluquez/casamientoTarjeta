35c6da523d8513465f06d922b6822c3015e443b1
Tomas luquez <luquez1431@gmail.com>
2023-08-25 12:39:31 -0300

    cambie color fondo

--- src/Components/General/Loading.tsx
+++ src/Components/General/Loading.tsx
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Loading = () => {
   return (
-    <div className="w-full bg-opacity-50 backdrop-blur-sm p-8 rounded-lg text-white text-5xl h-screen fixed top-0 left-0 flex justify-center items-center z-[5000]">
+    <div className="w-full bg-opacity-50 backdrop-blur-sm p-8 rounded-lg text-timer text-5xl h-screen fixed top-0 left-0 flex justify-center items-center z-[5000]">
       <FontAwesomeIcon icon={faCircleNotch} spin />
     </div>
   );

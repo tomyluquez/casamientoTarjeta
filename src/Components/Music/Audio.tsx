63ac8276f043168a7d2c6ff09ed401b2b6dd9100
Tomas luquez <luquez1431@gmail.com>
2023-10-21 14:56:58 -0300

    importe ruta cancion

--- src/Components/Music/Audio.tsx
+++ src/Components/Music/Audio.tsx
@@ -1,14 +1,13 @@
 import AudioPlayer from 'react-h5-audio-player';
 import 'react-h5-audio-player/lib/styles.css';
+import asi from '/asi.mp3';
 const Audio = () => {
   return (
     <div>
       <AudioPlayer
         autoPlay={true}
         loop={true}
-        src="/asi.mp3"
-        onPlay={(e) => console.log(e)}
-        onPause={(e) => console.log(e)}
+        src={asi}
         showJumpControls={false}
         autoPlayAfterSrcChange={false}
       />

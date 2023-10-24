08bafceda117786596e46dd5c25a7a7dc1606ea8
Tomas luquez <luquez1431@gmail.com>
2023-10-24 11:05:26 -0300

    cambie funcion add por set

--- src/Firebase/setInvitados.ts
+++ src/Firebase/setInvitados.ts
@@ -1,11 +1,17 @@
-import { getFirestore, collection, addDoc } from 'firebase/firestore';
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  setDoc,
+  doc,
+} from 'firebase/firestore';
 import { firebaseApp } from '../Firebase/firebase.config';
 import { Cancion, Invitado } from '../Types/types';
 
 export const setInvitado = (invitado: Invitado): Promise<any> => {
   const firestore = getFirestore(firebaseApp);
   const collectionRef = collection(firestore, 'invitados');
-  return addDoc(collectionRef, invitado);
+  return setDoc(doc(collectionRef, invitado.nombre), invitado);
 };
 
 export const setCancion = (cancion: Cancion): Promise<any> => {

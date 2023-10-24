005f60a78ca2f6451f27fd92e72cf8e344936566
Tomas luquez <luquez1431@gmail.com>
2023-10-23 21:15:06 -0300

    audio ios 2

--- src/Components/Music/Audios.tsx
+++ src/Components/Music/Audios.tsx
@@ -4,7 +4,7 @@ const AudioPlayer = () => {
   return (
     <>
       <div className="flex items-center justify-center my-4 audio-player">
-        <audio controls preload="auto" loop>
+        <audio controls preload="none" loop>
           <source src="./asi.mp3" type="audio/mpeg" />
           Your browser does not support the <code>audio</code> element.
         </audio>
